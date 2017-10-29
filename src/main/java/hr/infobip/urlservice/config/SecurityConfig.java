41e5b2981f3f7a9426a7e21d168867e15ca21dc6
filip <filip.kozjak@gmail.com>
2017-10-29 11:21:59 +0100

    Reindents config code

--- src/main/java/hr/infobip/urlservice/config/SecurityConfig.java
+++ src/main/java/hr/infobip/urlservice/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package hr.infobip.urlservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * Security configuration of this service.
+ * 
+ * @author fiilip
+ *
+ */
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
@@ -24,11 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		// permit public POST to '/account' endpoint
 		http.authorizeRequests()
-        .antMatchers(HttpMethod.POST, "/account") 
-          .permitAll().anyRequest().authenticated();
+							.antMatchers(HttpMethod.POST, "/account") 
+							.permitAll().anyRequest().authenticated();
 		
-		// TODO: investigate csrf
+		// disable csrf for security
 		http.csrf().disable();
 	}
 }
